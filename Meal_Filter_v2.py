cd7ff590dd135591d9849fc72b6142d0092c2cba
Mattyou <73671219+lordquinnington@users.noreply.github.com>
2021-02-16 16:23:59 +0000

    Add files via upload

--- Meal_Filter_v2.py
+++ Meal_Filter_v2.py
@@ -14,6 +14,7 @@ def filterProperty(prop,rProp):
 
 def retrievePossibleMeals(meals,filters):
     # takes the array of meals and filters required
+    # retrieves an array of the possible meals for each filter and puts them into one big array
 
     unfilteredMeals = []
 
@@ -21,21 +22,41 @@ def retrievePossibleMeals(meals,filters):
         tempArray1 = []
 
         for j in range(len(meals)):
-            tempArray2 = []
 
             for k in range(len(filters[i])):
             
                 if meals[j][0] not in unfilteredMeals:
                 
                     if filterProperty(filters[i][k],meals[j][i+1]):
-                        tempArray2.append(meals[j][0])
-
-            if len(tempArray2) != 0:
-                tempArray1.append(tempArray2)
+                        tempArray1.append(meals[j][0])
 
         unfilteredMeals.append(tempArray1)
 
     return unfilteredMeals
+
+
+def combineFilters(allPossibleMeals):
+    # takes the bog array of each possible meal for each filter
+    # returns an array with all the items matching the filters
+
+    potentialMeals = []
+
+    for i in range(len(allPossibleMeals)):
+
+        for j in range(len(allPossibleMeals[i])):
+            counter = 0     # counter to count if the value is in the other arrays of filtered items
+
+            for k in range(len(allPossibleMeals)):
+                
+                if allPossibleMeals[i][j] in allPossibleMeals[k]:
+                    counter += 1
+
+            if counter == len(allPossibleMeals):
+                potentialMeals.append(allPossibleMeals[i][j])
+
+    potentialMeals = list(set(potentialMeals))
+    
+    return potentialMeals
                     
 def readCSVFile():
     with open("Meals.csv") as file:
@@ -44,4 +65,6 @@ def readCSVFile():
     return array
 
 x = readCSVFile()
-print(retrievePossibleMeals(x,['y','rs','v']))
+y = retrievePossibleMeals(x,['n','s','pco'])
+print(y,"\n")
+print(combineFilters(y))
